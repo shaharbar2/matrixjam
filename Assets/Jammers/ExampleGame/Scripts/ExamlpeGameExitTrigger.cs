6d710995f07625de284bf28347d0e163fb2497b2
Oded Sharon <github@odedsharon.com>
2020-07-11 19:08:11 +0300

    finished example game

---
+++ Assets/Jammers/ExampleGame/Scripts/ExamlpeGameExitTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExamlpeGameExitTrigger : MonoBehaviour
+{
+    [SerializeField] private int exitNum;
+
+    [SerializeField] private UnityEvent exitEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Enter" + exitNum);
+        exitEvent.Invoke();
+    }
+    
+}
---
+++ Assets/Jammers/ExampleGame/Scripts/ExampleGameBallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleGameBallController : MonoBehaviour
+{
+
+    [SerializeField] private float Speed = 1;
+
+    [SerializeField] private Vector3 inputVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.z = Input.GetAxis("Vertical");
+        gameObject.GetComponent<Rigidbody>().AddForce(inputVector * Speed);
+    }
+}
