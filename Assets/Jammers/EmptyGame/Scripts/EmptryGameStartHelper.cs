9cf786368ebebff38b265860f2b5eb9b1966c78d
Oded Sharon <github@odedsharon.com>
2020-07-11 19:08:29 +0300

    added empty game

---
+++ Assets/Jammers/EmptyGame/Scripts/EmptryGameStartHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Basic_Matrix;
+using UnityEngine;
+
+public class EmptyGameStartHelper : StartHelper
+{
+       
+    public override void StartHelp(int num_ent)
+    {
+        // this is how the game starts
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
