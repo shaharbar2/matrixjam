6e923148e2c584ac710c74b47684501aa775c0d2
Oded Sharon <github@odedsharon.com>
2020-08-06 14:00:38 +0300

    added an exmaple for how to get the entrance  number.

---
+++ Assets/Jammers/EmptyGame/Scripts/StartBallGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatrixJam.Team
+{
+    public class StartBallGame : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Player Entered through entrance number:");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
